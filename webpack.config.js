f1e45d63af5731cc8bc699afb99473f5b1206586
James <zonakusu@gmail.com>
2017-02-13 17:41:24 +0100

    webpack init

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,88 @@
-var entry = './src/app/main.js',
-    output = {
-        path: __dirname,
-        filename: 'main.js'
-    };
+const { resolve }       = require('path');
+const webpack           = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports.development = {
-    debug : true,
-    devtool : 'eval',
-    entry: entry,
-    output: output,
-    module : {
-        loaders : [
-            { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' },
+module.exports = [
+    {
+        entry: {
+            'webpack-dev-server/client?http://localhost:8080': 'webpack-dev-server/client?http://localhost:8080',
+            'webpack/hot/only-dev-server': 'webpack/hot/only-dev-server',
+            'main': './main.js',
+        },
+
+        output: {
+            filename: '[name].js',
+            path: resolve(__dirname, 'build/js'),
+            // publicPath: 'build',
+        },
+
+        resolve: {
+            modules: ['./src/js', 'node_modules'],
+            extensions: ['.js', '.json'],
+            // alias: {
+            //     handlebars: 'handlebars/dist/handlebars.min.js'
+            // }
+        },
+
+        context: resolve(__dirname, 'src/js'),
+
+        // devtool: 'inline-source-map',
+
+        devServer: {
+            hot: true,
+            contentBase: resolve(__dirname, 'src'),
+            // publicPath: '/',
+            host: '0.0.0.0',
+            historyApiFallback: true,
+        },
+
+        module: {
+            loaders: [
+                {
+                    test: /\.(js|jsx)$/,
+                    loaders: [
+                        'babel-loader'
+                    ],
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader',
+                    query: {
+                        // minimize: true
+                    }
+                }
+            ],
+        },
+
+        performance: {
+            hints: false
+        },
+
+        plugins: [
+            // new webpack.optimize.UglifyJsPlugin(),
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NamedModulesPlugin(),
+        ],
+    },{
+        entry: resolve(__dirname, 'src/css/main.css'),
+
+        output: {
+            path: resolve(__dirname, 'build/css'),
+            filename: 'main.css'
+        },
+
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract({ loader: 'css-loader?importLoaders=1!postcss-loader' }),
+                }
+            ]
+        },
+
+        plugins: [
+            new ExtractTextPlugin("[name].css")
         ]
     }
-};
-
-module.exports.production = {
-    debug: false,
-    entry: entry,
-    output: output,
-    module : {
-        loaders : [
-            { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' },
-        ]
-    }
-};
+];
---
+++ webpack.dist.config.js
@@ -0,0 +1,78 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports =
+    [{
+        entry: [
+            './main.js',
+        ],
+
+        output: {
+            filename: 'main.js',
+            path: './docs/js',
+            publicPath: '/',
+        },
+
+        resolve: {
+            modules: ['./src/js', 'node_modules'],
+            extensions: ['.js', '.json']
+        },
+
+        context: resolve(__dirname, 'src/js'),
+
+        devtool: "source-map",
+
+        module: {
+            loaders: [
+                {
+                    test: /\.(js|jsx)$/,
+                    loaders: [
+                        'babel-loader'
+                    ],
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader',
+                    query: {
+                        minimize: true
+                    }
+                }
+            ],
+        },
+
+        node: {
+            console: true,
+            fs: 'empty'
+        },
+
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'NODE_ENV': JSON.stringify('production')
+                }
+            }),
+        ]
+    },{
+        entry: resolve(__dirname, 'src/css/main.css'),
+
+        output: {
+            path: resolve(__dirname, 'docs/css'),
+            filename: 'main.css'
+        },
+
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract({ loader: 'css-loader?importLoaders=1!postcss-loader' }),
+                }
+            ]
+        },
+
+        plugins: [
+            new ExtractTextPlugin("[name].css")
+        ]
+    }
+];
